35ab9e25fe7c57a67781c8b8f49bbeb8e30a7860
BrennerFarias <brennerfariasds@gmail.com>
2025-08-26 16:16:50 -0300

    arvore de natal py 2.0

--- Arvore_de_Natal.py
+++ Arvore_de_Natal.py
@@ -2,49 +2,68 @@ import os
 import platform
 import time
 
+#Função para limpar tela
 def limpa_tela():
     if platform.system().lower() == "windows":
         os.system("cls")
     else:
         os.system("clear")
 
+#loop para manter o sistema funcionando até o usuário n querer mais.
 while True:
     limpa_tela()
     tamanho = int(input("Qual o tamanho da arvore: "))
-    tronco = int(max(1, tamanho / 2))
     galhos = 1
+    tronco = 0
+
+    if tamanho // 2 - 1 > 1:
+        tronco = tamanho // 2 - 1
+    else:
+        tronco = 1
+
     limpa_tela()
 
+    #desenho da arvore
     for i in range(tamanho):    #define a altura da arvore.
         for j in range(tamanho - i - 1):    #dá espaçamentos.
             print(" ", end="")
     
         for k in range(galhos):     #coloca os galhos.
-            print("o", end="")
+            print("*", end="")
 
         #lógica para fazer com q os galhos aumentem gradativamente.
         galhos = galhos + 1 * 2
         print("")
 
-    espacos_tronco = tamanho - 1  
+    #lógica para por os espaços do tronco da arvore.
+    espacos_tronco = int(tamanho - tronco / 2)
 
-    for j in range(espacos_tronco):
+    #fazer o desenho do tronco
+    for j in range(espacos_tronco): #coloca os espaçamentos.
         print(" ", end="")
 
-    for i in range(tronco):
+    for i in range(tronco): # serve para aplicar o tronco em si.
         print("|", end="")
 
+    #função para saber se o usuário que continuar o programa.
     time.sleep(2)
     while True:
         escolha = int(input("\n\nGostaria de fazer de novo?\n1 - Sim.\n2 - Não.\n\n>> "))
         
-        if escolha == 2:
+        if escolha == 1:
+            break
+        elif escolha == 2:
             limpa_tela()
             print("Encerrando Programa")
-            break
-        elif escolha != 1 and escolha != 2:
+            for i in range(0, 6, 1):
+                print(" o", end="", flush=True)
+                time.sleep(1)
+            exit()
+        else:
+        #elif escolha != 1 and escolha != 2:
             limpa_tela()
             print(f"Ops... A opção {escolha} é inválida!")
             time.sleep(1)
-            print("\nClique qualquer tecla para continuar.")
+            input("\nClique qualquer tecla para continuar.")
+            limpa_tela()
     
--- README.md
+++
@@ -1,4 +0,0 @@
-# Hello my dear programmer!!!
-'''Esse repositório esta destinado a receber apenas resoluções, em diferentes 
-   linguagens de programação, de desafios para testar a lógica de programação,
-   testes esses podendo ser de diversos tipos, como a clássica Árvore de Natal.'''
